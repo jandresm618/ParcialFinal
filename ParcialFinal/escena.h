e8597f75d9d8f76ebc81c1c51530017829bd8450
jandresm618 <jandres.mosquera@udea.edu.co>
2021-02-04 10:01:20 -0500

    Labores Personales y Pequeños ajustes

--- ParcialFinal/escena.h
+++ ParcialFinal/escena.h
@@ -16,15 +16,10 @@ public:
     Escena_Juego();
     ~Escena_Juego();
 
-        /// MOVIMIENTO DE MOUSE
-    void mouseMoveEvent(QGraphicsSceneMouseEvent *event) ;
 
         /// CARACTERISTICAS DE ESCRITORIO
     void setWindowProperty(int desk_w, int desk_h);
     virtual void drawBackground(QPainter *painter, const QRectF &exposed);
-
-
-    void arcadeDesing();
 
         /// FUNCIONES AÑADIR OBJETOS GRAFICOS
     void addObjetoGrafico(QString ruta,int x,int y,int w,int h);
@@ -32,14 +27,6 @@ public:
     void addObjetoMovil(QString ruta,int x,int y,int v0,int angle,int move);
     void explodeObject(int _x,int _y,int _w,int _h);
 
-        /// FUNCIONES DE PRUEBA
-    void doSome();
-
-        /// MANEJO DE ATRIBUTO SANGRE
-    void setHurt();
-    int getHurt();
-    int getBlood() const;
-    void setBlood(int value);
 
         /// FUNCION DE CONTROL DE JUEGO
     void pause();
@@ -49,22 +36,15 @@ public:
         /// CONTROL DE COLISIONES Y PUESTAS EN ESCENA
     bool deleteFromScene();
 
-        /// MANEJO DE ATRIBUTO SCORE
-    int getScore() const;
-    void setScore(int value);
-    void setScorePlus();
 
-
-    vector<Objeto_Movil *> getObjetosMoviles() const;
 
 private:
     /// APUNTADOR A OBJETOS GRAFICOS
-    Objeto_Grafico *personaje;
-    Objeto_Movil *muni;
+    Objeto_Grafico *torres;
+    Objeto_Movil *proyectiles;
 
         /// CONTENEDORES DE OBJETOS
     vector<Objeto_Grafico *> objetosGraficos;
-    vector<Objeto_Grafico *>::iterator itObjGra;
     vector<Objeto_Movil *> objetosMoviles;
     vector<Objeto_Movil *>::iterator itObjMov;
     vector<Objeto_Movil *>::iterator itObjMov2;
@@ -74,9 +54,6 @@ private:
     QPixmap *image;
 
         /// ATRIBUTOS DE JUEGO
-    int blood = 100;
-    int score = 0;
-    int cont_1 = 0;
     int time_move = 60;
 };
 
